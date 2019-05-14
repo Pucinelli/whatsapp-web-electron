4e8d870085dc8953e597c521f252383bb681c216
Gabriel Pucinelli <gabrielpucinelli@gmail.com>
2019-05-14 20:20:45 -0300

    Add image to window

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const { join } = require('path');
 
 let win;
 
 app.on('ready', () => {
     win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        icon: join(__dirname, process.platform == 'win32' ? './whatsapp-web-electron.ico' : './whatsapp-web-electron.png')
     });
     win.loadURL('https://web.whatsapp.com/', {
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'
